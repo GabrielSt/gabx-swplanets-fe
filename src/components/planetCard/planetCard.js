eda9f26c37c0c3d160bc6618a471174c0cb6891c
Gabriel Santos <gabriel.st@outlook.com>
2019-08-28 01:07:31 -0300

    Initial commit

---
+++ src/components/planetCard/planetCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+const Card = () => {
+  return (
+    <div id="card">
+      <div id="front">
+        <div id="top-pic"></div>
+        <div id="info-box">
+          <div class="info">
+            <h1>Jesse Couch</h1>
+            <h2>Digital Creative</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
